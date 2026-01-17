08586f5c8cdea347533a47502eb14b7cf05cbedb
glyphor-ai <glyphor-ai@noreply.github.com>
2026-01-17 22:46:27 +0000

    ai: repair - build (attempt 1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-// Suggestion: Install 'react-router-dom' and '@types/react-router-dom' in package.json
-// @ts-expect-error - react-router-dom types may not be installed
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
 import Navbar from './components/Navbar';
 import GlyphorBadge from './utils/GlyphorBadge';
 
 function App() {
   return (
-    <Router>
-      <div className="relative min-h-screen bg-arctic-950 text-white font-sans">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Index />} />
-        </Routes>
-        <div className="fixed bottom-8 right-8 z-50 mix-blend-difference pointer-events-none">
-          <GlyphorBadge />
-        </div>
+    <div className="relative min-h-screen bg-arctic-950 text-white font-sans">
+      <Navbar />
+      <Index />
+      <div className="fixed bottom-8 right-8 z-50 mix-blend-difference pointer-events-none">
+        <GlyphorBadge />
       </div>
-    </Router>
+    </div>
   );
 }
 
